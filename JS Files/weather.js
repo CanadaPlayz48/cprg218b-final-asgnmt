3ea5f26278771b455474ef45caae3f17c989cd29
CanadaPlayz48 <radznag3354@gmail.com>
2025-04-12 17:18:12 -0600

    added more formatting and consistent header

--- JS Files/weather.js
+++ JS Files/weather.js
@@ -6,8 +6,8 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=66321ba752
     console.log(data.main.temp)
     console.log(data.weather[0].description)
     document.getElementById("temp").innerHTML = "Temperature: "+ data.main.temp +"&#xb0;C"
-    document.getElementById("desc").innerHTML = "Currently: "+data.weather[0].description+")"
-    document.getElementById("wind").innerHTML = "Wind: "+(data.wind.speed*3.6)+ " km/h"
+    document.getElementById("desc").innerHTML = "Currently: "+data.weather[0].description
+    document.getElementById("wind").innerHTML = "Wind: "+(data.wind.speed)+ " m/s"
     document.getElementById("tempMin").innerHTML = "Today's Low: "+data.main.temp_min+"&#xb0;C"
     document.getElementById("tempMax").innerHTML = "Today's High: "+data.main.temp_max+"&#xb0;C"
     document.getElementById("tempFL").innerHTML = "Feels Like: "+data.main.feels_like+"&#xb0;C"
