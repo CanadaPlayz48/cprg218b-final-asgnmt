723a5de2823221ecbfed9145a1dcfe602e8e7dc1
CanadaPlayz48 <radznag3354@gmail.com>
2025-04-14 00:28:06 -0600

    Validated JS

--- JS_Files/weather.js
+++ JS_Files/weather.js
@@ -3,16 +3,13 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=66321ba752
 
 .then(response => response.json())
 .then (data => {
-    console.log(data)
-    console.log(data.main.temp)
-    console.log(data.weather[0].description)
-    document.getElementById("temp").innerHTML = "Temperature: "+ data.main.temp +"&#xb0;C"
-    document.getElementById("desc").innerHTML = "Currently: "+data.weather[0].description
-    document.getElementById("wind").innerHTML = "Wind: "+(data.wind.speed)+ " m/s"
-    document.getElementById("icon").src = "https://openweathermap.org/img/w/"+data.weather[0].icon+".png"
-    document.getElementById("updated").innerHTML = "Last Updated: "+new Date(data.dt*1000).toLocaleTimeString()
-    document.getElementById("updated").style.fontSize = "10px"
-
-    
-
-})
+    console.log(data);
+    console.log(data.main.temp);
+    console.log(data.weather[0].description);
+    document.getElementById("temp").innerHTML = "Temperature: "+ data.main.temp +"&#xb0;C";
+    document.getElementById("desc").innerHTML = "Currently: "+data.weather[0].description;
+    document.getElementById("wind").innerHTML = "Wind: "+(data.wind.speed)+ " m/s";
+    document.getElementById("icon").src = "https://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+    document.getElementById("updated").innerHTML = "Last Updated: "+new Date(data.dt*1000).toLocaleTimeString();
+    document.getElementById("updated").style.fontSize = "10px";
+});
