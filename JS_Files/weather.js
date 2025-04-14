d9d9d73810da1be27a369b830dcdd77a34ee0c56
CanadaPlayz48 <radznag3354@gmail.com>
2025-04-13 23:24:34 -0600

    JS Fetch Fix 3

--- JS_Files/weather.js
+++ JS_Files/weather.js
@@ -1,5 +1,5 @@
 
-fetch("http://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=66321ba752eb88b22f3de6db767a89a5&units=metric")
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=66321ba752eb88b22f3de6db767a89a5&units=metric")
 
 .then(response => response.json())
 .then (data => {
@@ -9,7 +9,7 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=66321ba752e
     document.getElementById("temp").innerHTML = "Temperature: "+ data.main.temp +"&#xb0;C"
     document.getElementById("desc").innerHTML = "Currently: "+data.weather[0].description
     document.getElementById("wind").innerHTML = "Wind: "+(data.wind.speed)+ " m/s"
-    document.getElementById("icon").src = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png"
+    document.getElementById("icon").src = "https://openweathermap.org/img/w/"+data.weather[0].icon+".png"
     document.getElementById("updated").innerHTML = "Last Updated: "+new Date(data.dt*1000).toLocaleTimeString()
     document.getElementById("updated").style.fontSize = "10px"
 
